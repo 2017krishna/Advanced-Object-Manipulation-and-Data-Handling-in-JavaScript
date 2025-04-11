7366bcd29e58ae340634caabfd67cc952a4f8c2f
krishna-2017 <krishna_2017@hotmail.com>
2025-04-10 22:38:59 -0400

    Part 2

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ console.log(`Name: ${student.name}, Age: ${student.age}`);
 
 console.log(student.displayInfo());
 
+const studentJSON = JSON.stringify(student);
+
+
+console.log("JSON String:", studentJSON);
+
+const parsedStudent = JSON.parse(studentJSON);
+
+console.log("Parsed Object:", parsedStudent);
+console.log("Are the objects equal?", JSON.stringify(student) === JSON.stringify(parsedStudent));
